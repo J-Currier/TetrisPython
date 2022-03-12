ef406306fbdb66512f38432911185b986e91d7b3
unknown <j.currier224@gmail.com>
2022-03-11 22:29:36 -0700

    grid color function

--- main.py
+++ main.py
@@ -147,7 +147,16 @@ class Piece(object):
         self.rotation = 0
  
 def create_grid(locked_positions={}):
-    pass
+    #(0, 0, 0,) black create a list of ten (width) for each row (20)
+    #columns = j, rows = i 
+    grid = [[(0, 0, 0)for x in range(10) for x in range(20)]
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if(j, i) in locked_pos:
+                c = locked_pos[j, i)]
+                grid[i][j] = c
+    return grid
  
 def convert_shape_format(shape):
     pass
